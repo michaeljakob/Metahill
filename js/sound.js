f714e8f2dfbbac899e526985a4930be6b7d263a9
MyMichael <michaeljakob@Michaels-MacBook-Pro.local>
2013-06-26 11:30:40 +0200

    Search input fields have appropriate handlers
    Those handlers have not been implemented yet.
    Some lines in init.js have been removed as regarded superflous (e.g. a
    window.resize call)

--- js/sound.js
+++ js/sound.js
@@ -5,14 +5,14 @@ function __sound__(modals) {
 
     this.playUserAddressed = function() {
         var audioTag = document.createElement('audio');
-        audioTag.setAttribute('src', 'sound/notification.mp3');
+        audioTag.setAttribute('src', 'sound/user-addressed.mp3');
 
         return function() {
             if(modals.preferences.enable_notification_sounds){
                 audioTag.play();
             }
-        }();
-    };
+        };
+    }();
     
 }
 
