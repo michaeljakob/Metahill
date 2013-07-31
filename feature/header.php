93ea5ea35c2d39e484300bc73a68cc9c5d590817
MyMichael <michaeljakob@Michaels-MacBook-Pro.local>
2013-07-31 10:58:19 +0200

    magnify image links
    - images don't always are 100px heigh, 100px is the max height
    - embedded code

--- feature/header.php
+++ feature/header.php
@@ -3,34 +3,36 @@
 
         
     <?php
-        if(isset($_SESSION['name']) && isset($_SESSION['verified']) && isset($_SESSION['logged_in']) && strpos($_SERVER['PHP_SELF'], 'index.php') !== false) {
-            $name = $_SESSION['name'];
-            // $user already set in index.php
-            $userId = $user->id;
-            
-            echo '<div class="menu">
-                    <div id="site-status" class="alert">
-                        Some status message here
-                    </div>';
+        if( isset($_SESSION['name']) && 
+            isset($_SESSION['verified']) && 
+            isset($_SESSION['logged_in']) && 
+            strpos($_SERVER['PHP_SELF'], 'index.php') !== false) {
+              $name = $_SESSION['name'];
+              // $user already set in index.php
+              $userId = $user->id;
+              
+              echo '<div class="menu">
+                      <div id="site-status" class="alert">
+                          Some status message here
+                      </div>';
 
-            // avatar
-            echo "<div id='user-id' style='display:none;'>$userId</div>";
-            
-            // user
-            echo '<div id="userbar">
-                    <div class="btn-group" id="user-name">
-                        <button class="btn dropdown-toggle" data-toggle="dropdown">'. $name .'&nbsp;<span class="caret"></span></button>
-                        <ul class="dropdown-menu pull-right">
-                          <li><a href="#modal-pref" data-toggle="modal">Preferences</a></li>
-                          <li><a href="#modal-profile" data-toggle="modal">Profile</a></li>
-                          <li><a href="help" target="_blank">Help</a></li>
-                          <li><a href="forum" target="_blank">Forum</a></li>
-                          <li class="divider"></li>
-                          <li><a href="logout.php">Sign out</a></li>
-                        </ul>
+              echo "<div id='user-id' style='display:none;'>$userId</div>";
+              
+              // user
+              echo '<div id="userbar">
+                      <div class="btn-group" id="user-name">
+                          <button class="btn dropdown-toggle" data-toggle="dropdown">'. $name .'&nbsp;<span class="caret"></span></button>
+                          <ul class="dropdown-menu pull-right">
+                            <li><a href="#modal-pref" data-toggle="modal">Preferences</a></li>
+                            <li><a href="#modal-profile" data-toggle="modal">Profile</a></li>
+                            <li><a href="help" target="_blank">Help</a></li>
+                            <li><a href="forum" target="_blank">Forum</a></li>
+                            <li class="divider"></li>
+                            <li><a href="logout.php">Sign out</a></li>
+                          </ul>
+                        </div>
                       </div>
-                    </div>
-                  </div>';
+                    </div>';
 
         }
     ?>
